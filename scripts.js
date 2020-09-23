e0e0f21502b843350ee0dbc5fe3ebfe6d6b1f572
anonyayayaya <71608652+anonyayayaya@users.noreply.github.com>
2020-09-23 01:06:56 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,9 +4,13 @@ var precinct_avail;
 
 function checkRegistrationCode() {
     var registrationcode = document.getElementById("registrationCodeInput").value;
-    if (registrationcode == '19kjjna*&D389js09AFJ4%^olr2w43o9ia')
+    if (registrationcode == '19kjjna*&D389jsl09AFJ4%^olr2w43o9ia')
         document.write("Oops, that's not a valid registration code. Please try again and make sure you can differentiate your capital i's and lowercase l's.");
-    else if (registrationcode != '19kjjna*&D389js09AFJ4%^oIr2w43o9ia')
+    else if (registrationcode == '19kjjna*&D389jsI09AFJ4%^oIr2w43o9ia')
+        document.write("Oops, that's not a valid registration code. Please try again and make sure you can differentiate your capital i's and lowercase l's.");
+    else if (registrationcode == '19kjjna*&D389jsI09AFJ4%^olr2w43o9ia')
+        document.write("Oops, that's not a valid registration code. Please try again and make sure you can differentiate your capital i's and lowercase l's.");
+    else if (registrationcode != '19kjjna*&D389jsl09AFJ4%^oIr2w43o9ia')
         document.write("Oops, that's not a valid registration code. Please try again");
     else
         window.location.href = './choosefavactivity.html';
@@ -40,8 +44,114 @@ function precinctClick(num) {
         alert("Sorry, this precinct is full. Please choose another location.");
 }
 
+function submitFinalQuiz1() {
+    let formValid = true;
+    if (!(document.getElementById("q1t").checked) && (document.getElementById("q1f").checked)) {
+        alert("Sorry, that's not right. Please select the right answers according to the information provided in the previous page. You must show that you're well informed to vote before registration is complete. Remember that you can vote before election day.");
+        formValid = false;
+    }
+    else if (!document.getElementById("q2t1").checked) {
+        alert("Sorry, that's not right. Please select the right answers according to the information provided in the previous page. You must show that you're well informed to vote before registration is complete.");
+        formValid = false;
+    }
+    else if (!document.getElementById("q2t2").checked) {
+        alert("Sorry, that's not right. Please select the right answers according to the information provided in the previous page. You must show that you're well informed to vote before registration is complete.");
+        formValid = false;
+    }
+    else if (!document.getElementById("q2t3").checked) {
+        alert("Sorry, that's not right. Please select the right answers according to the information provided in the previous page. You must show that you're well informed to vote before registration is complete. Remember that only IDs from US institutions are valid.");
+        formValid = false;
+    }
+    else if (document.getElementById("q2f1").checked) {
+        alert("Sorry, that's not right. Please select the right answers according to the information provided in the previous page. You must show that you're well informed to vote before registration is complete.");
+        formValid = false;
+    }
+    else if (document.getElementById("q2f2").checked) {
+        alert("Sorry, that's not right. Please select the right answers according to the information provided in the previous page. You must show that you're well informed to vote before registration is complete.");
+        formValid = false;
+    }
+    else if (document.getElementById("q2f3").checked) {
+        alert("Sorry, that's not right. Please select the right answers according to the information provided in the previous page. You must show that you're well informed to vote before registration is complete. Remember that utility bills include things like water and electricity.");
+        formValid = false;
+    }
+    else if (document.getElementById("q2f4").checked) {
+        alert("Sorry, that's not right. Please select the right answers according to the information provided in the previous page. You must show that you're well informed to vote before registration is complete.");
+        formValid = false;
+    }
+    else if (document.getElementById("q2f5").checked) {
+        alert("Sorry, that's not right. Please select the right answers according to the information provided in the previous page. You must show that you're well informed to vote before registration is complete.");
+        formValid = false;
+    }
+    if (formValid == true) {
+        window.location.href = './finalquiz2.html';
+    }
+}
+
+function submitFinalQuiz2() {
+    let formValid = true;
+    if (!(document.getElementById("q1t").checked) && (document.getElementById("q1f").checked)) {
+        formValid = false;
+        alert("Sorry, that's not right. If you don't bring your ID, you will be asked to sign a statement that you are the registered voter you claim to be.");
+    }
+    if (formValid == true) {
+        window.location.href = './finalquiz3.html';
+    }
+}
+
+function submitFinalQuiz3() {
+    let formValid = true;
+    if (document.getElementById("sign").value != "sign") {
+        formValid = false;
+        alert("Please fill out the fields accurately and exactly.");
+    }
+    if (document.getElementById("statement").value != "statement") {
+        formValid = false;
+        alert("Please fill out the fields accurately and exactly.");
+    }
+    if (document.getElementById("registered").value != "registered") {
+        formValid = false;
+        alert("Please fill out the fields accurately and exactly.");
+    }
+    if (document.getElementById("noon").value != "noon") {
+        formValid = false;
+        alert("Please fill out the fields accurately and exactly.");
+    }
+    if (document.getElementById("Friday").value != "Friday") {
+        formValid = false;
+        alert("Please fill out the fields accurately and exactly.");
+    }
+    if (document.getElementById("local").value != "local") {
+        formValid = false;
+        alert("Please fill out the fields accurately and exactly.");
+    }
+    if (document.getElementById("electoral").value != "electoral") {
+        formValid = false;
+        alert("Please fill out the fields accurately and exactly.");
+    }
+    if (document.getElementById("board").value != "board") {
+        formValid = false;
+        alert("Please fill out the fields accurately and exactly.");
+    }
+    if (formValid == true) {
+        window.location.href = './finalquiz4.html';
+    }
+}
+
+function submitFinalQuiz4() {
+    let formValid = true;
+    if (!(document.getElementById("q1t").checked) && (document.getElementById("q1f").checked)) {
+        formValid = false;
+        alert("Sorry, that's not right. Remember that The early voting period runs from Friday, September 18, 2020 to Saturday, October 31, 2020, but dates and hours may vary based on where you live.");
+    }
+    console.log(document.getElementById("sept18").value == '2020-09-18');
+    console.log(document.getElementById("oct31").value == '2020-10-31');
+    if (formValid == true) {
+        window.location.href = './done.html';
+    }
+}
+
 function submitFinalForm() {
-    var formValid = true;
+    let formValid = true;
     if (document.forms["finalForm"]["firstname"].value.length == 0) {
         document.getElementById("finalFormErrors").innerHTML = "Please fill out the missing input.";
         formValid = false;
@@ -87,7 +197,7 @@ function submitFinalForm() {
     }
 
     if (formValid) {
-        window.location.href = './done.html';
+        window.location.href = './details.html';
     }
 
 }
