9e308db2bc5bf22aa51e6b9e488d5f1a01856a85
anonyayayaya <71608652+anonyayayaya@users.noreply.github.com>
2020-09-28 23:14:31 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,13 +4,13 @@ var precinct_avail;
 
 function checkRegistrationCode() {
     var registrationcode = document.getElementById("registrationCodeInput").value;
-    if (registrationcode == 'jsl09AFJ4%^olr')
+    if (registrationcode == '9jsl09AFJ4%^olr')
         document.write("Oops, that's not a valid registration code. Please try again and make sure you can differentiate your capital i's and lowercase l's.");
-    else if (registrationcode == 'jsI09AFJ4%^oIr')
+    else if (registrationcode == '9jsI09AFJ4%^oIr')
         document.write("Oops, that's not a valid registration code. Please try again and make sure you can differentiate your capital i's and lowercase l's.");
-    else if (registrationcode == 'jsI09AFJ4%^olr')
+    else if (registrationcode == '9jsI09AFJ4%^olr')
         document.write("Oops, that's not a valid registration code. Please try again and make sure you can differentiate your capital i's and lowercase l's.");
-    else if (registrationcode != 'jsl09AFJ4%^oIr')
+    else if (registrationcode != '9jsl09AFJ4%^oIr')
         document.write("Oops, that's not a valid registration code. Please try again");
     else
         window.location.href = './choosefavactivity.html';
